07353f4790c19251e35bbc5dbac8c445149406aa
ponpon <wizardsoloist1983@gmail.com>
2017-01-11 14:59:07 +0800

    add hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 var path = require('path');
 module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'src/main.js')],
-    output: {
-        filename: './dist/bundle.js'
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx', '.css']
-    },
-    module: {
+  devServer: {
+    host: 'localhost',
+    port: '8080', // 默认webpack是用 8080 端口起的，通过port可以配成其他的端口。
+    hot: true,  // 熱加載。
+    inline: true, // 设置为true，代码有变化，浏览器端刷新。
+    historyApiFallback: true,
+  },
+  entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'src/main.js')],
+  output: {
+    filename: './dist/bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.css$/,
         loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel']
-            },
-            {
-              test: /\.css$/,
-              loaders: [
-                  'style?sourceMap',
-                  'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
-              ]
-            }
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
         ]
-    }, devtool: 'source-map'
+      }
+    ]
+  }, devtool: 'source-map'
 };
