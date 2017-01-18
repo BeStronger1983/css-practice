d77daaf6a8277d733faab14f78fe9ad5596e67dc
ponpon <wizardsoloist1983@gmail.com>
2017-01-18 17:30:26 +0800

    add half_pie

--- src/containers/PieChart/index.jsx
+++ src/containers/PieChart/index.jsx
@@ -14,6 +14,8 @@ class PieChart extends React.Component {
   render() {
     return (
       <div className={styles.divRoot}>
+        <div className={styles.half_pie}>
+        </div>
         <div className={styles.pie_chart}>
         </div>
         <div className={styles.no_padding_no_border_box}>
